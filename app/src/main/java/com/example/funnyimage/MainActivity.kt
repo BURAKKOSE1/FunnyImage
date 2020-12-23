55f8456530799b59497091df3ba79dc393e3af73
BURAKKOSE1 <63636134+BURAKKOSE1@users.noreply.github.com>
2020-12-23 22:43:02 +0300

    Funny Image Add

---
+++ app/src/main/java/com/example/funnyimage/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.funnyimage
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            val intent = Intent(applicationContext,FeedActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+    fun signInClicked(view : View) {
+
+        auth.signInWithEmailAndPassword(userEmailText.text.toString(),passwordText.text.toString()).addOnCompleteListener { task ->
+
+            if (task.isSuccessful) {
+                //Signed In
+                Toast.makeText(applicationContext,"Welcome: ${auth.currentUser?.email.toString()}",Toast.LENGTH_LONG).show()
+                val intent = Intent(applicationContext,FeedActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
+
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext,exception.localizedMessage.toString(), Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    fun signUpClicked(view : View) {
+
+        val email = userEmailText.text.toString()
+        val password = passwordText.text.toString()
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+
+            if (task.isSuccessful) {
+                val intent = Intent(applicationContext,FeedActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }.addOnFailureListener { exception ->
+            if (exception != null) {
+                Toast.makeText(applicationContext,exception.localizedMessage.toString(),Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+}
